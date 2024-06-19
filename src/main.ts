88538b29e7b7afdf2c9e7a374b7ebf87dbd73b71
ZarakiMelo <chahbisamy@hotmail.fr>
2024-06-19 11:47:08 +0200

    Backend structure

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+console.log("New app")
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
